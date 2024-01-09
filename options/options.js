f86ee63cfce055077ecb9576d22b015342f7a79f
Crowdbotics <team@crowdbotics.com>
2024-01-09 12:46:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-salad-45329",
+  url: "https://billowing-salad-45329.botics.co",
+  api: "https://billowing-salad-45329.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
